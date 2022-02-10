d2cda9a0452b0ca3bb5d0989b54d9c19101766e3
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-02-10 12:53:00 +0300

    compiler options in tsconfig.json

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+const message: string = 'Hello world'
+
+class Person {
+  constructor(private name: string) { }
+}
+
+const max = new Person('Maxim')
+
+const btn = document.querySelector('#btn')!
+
+btn.addEventListener('click', () => {
+  console.log('Button clicked')
+})
