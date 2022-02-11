df4a98a3f58cff26f79a986bfab77c8cd59727e7
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-02-11 20:14:59 +0300

    add mergeObjects examle generic.ts

--- src/generic.ts
+++ src/generic.ts
@@ -2,12 +2,21 @@
 // // либо
 // const cars2: Array<string> = ['Ford', 'Audi']
 
-const promise = new Promise<string>(resolve => {
-  setTimeout(() => {
-    resolve('Promise resolved')
-  }, 1000)
-})
+// const promise = new Promise<string>(resolve => {
+//   setTimeout(() => {
+//     resolve('Promise resolved')
+//   }, 1000)
+// })
 
-promise.then(data => {
-  console.log(data.toLowerCase().trim())  
-})
+// promise.then(data => {
+//   console.log(data.toLowerCase().trim())  
+// })
+
+function mergeObjects<T, R>(a: T, b: R) {
+  return Object.assign({}, a, b)
+}
+
+const merged = mergeObjects({ name: 'Victor' }, { age: 30 })
+const merged2 = mergeObjects({ model: 'Niva' }, { year: 2020 })
+
+console.log(merged)
