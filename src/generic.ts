7696640a32d2f41c84a931a5ae82a1065960fd34
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-02-12 05:46:00 +0300

    add extends examle generic.ts

--- src/generic.ts
+++ src/generic.ts
@@ -12,11 +12,32 @@
 //   console.log(data.toLowerCase().trim())  
 // })
 
-function mergeObjects<T, R>(a: T, b: R) {
+
+// функция работает 
+function mergeObjects<T extends object, R extends object>(a: T, b: R) {
   return Object.assign({}, a, b)
 }
 
 const merged = mergeObjects({ name: 'Victor' }, { age: 30 })
 const merged2 = mergeObjects({ model: 'Niva' }, { year: 2020 })
 
-console.log(merged)
+// const merged3 = mergeObjects('Niva', 2020 ) // не корректно, т.к. функция работает только с объектами
+
+// console.log(merged)
+
+// ====
+interface Ilength {
+  length: number
+}
+
+function withCount<T extends Ilength>(value: T): { value: T, count: string } {
+  return {
+    value,
+    count: `В этом объекте ${value.length} символов`
+  }
+}
+
+console.log(withCount('Hello Typescript'))
+console.log(withCount([1, 2, 3, 4, 7]))
+// console.log(withCount(1)) // ошибка
+console.log(withCount({ length: 4 })) // ошибка
