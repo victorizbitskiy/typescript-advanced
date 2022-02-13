c9d8a2d2d340bd566128e9a72ec5e0358ce8d777
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-02-13 19:33:47 +0300

    add collection examle generic.ts

--- src/generic.ts
+++ src/generic.ts
@@ -57,3 +57,37 @@ console.log(getObjectValue(person, 'name'))
 console.log(getObjectValue(person, 'age'))
 console.log(getObjectValue(person, 'job'))
 
+// ====
+
+class Collection<T extends number | string | boolean> {
+  constructor(private _items: T[] = []) { }
+
+  add(item: T) {
+    this._items.push(item)
+  }
+
+  remove(item: T) {
+    this._items = this._items.filter(i => i !== item)
+  }
+
+  get items(): T[] {
+    return this._items
+  }
+}
+
+const strings = new Collection<string>(['I', 'Am', 'Strings'])
+strings.add('!')
+strings.remove('Am')
+console.log(strings.items)
+
+const numbers = new Collection<number>([1, 2, 3])
+numbers.add(2)
+numbers.remove(2)
+console.log(numbers.items)
+
+// Объекты в эту коллекцию не сохранить, 
+// т.к. если передать в remove объект, то это будет новый объект, а не ссылка на существующий в коллекции
+
+// const objects = new Collection<object>({ a: 1 }, { b: 2 })
+// objects.remove({a: 1})
+// console.log(objects.items)
